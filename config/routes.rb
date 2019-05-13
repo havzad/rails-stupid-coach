d6854ce845489cf2660933c021463c136e515678
aaron feldman <aaronalex.feldman@gmail.com>
2019-05-13 15:58:11 -0400

    hello world'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/questions/ask', to: 'questions#ask'
+  get '/questions/answer', to: 'questions#answer'
+  get '/answer', to: 'questions#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
